f7c9b24530ab05017ac830d7ecca6acc56da2832
AGNESGITAU <agnesgitau40@gmail.com>
2024-05-04 05:33:26 +0300

    routes

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar";
+
+function ErrorPage(){
+    return (
+        <>
+        <header>
+            <NavBar />
+        </header>
+        <main>
+            <h1>Ooops! Looks like something went wrong.</h1>
+        </main>
+        </>
+    )
+}
+export default ErrorPage
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,34 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar"
+import { useParams } from "react-router-dom";
+
 
 function Movie() {
+  const [movie, setMovie] =useState({})
+  const params = useParams();
+  const movieId = params.id
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then(r => r.json())
+    .then(data => setMovie(data))
+    .catch(error=>console.error(error))
+
+  }, [movieId])
+
+  if(!movie.title){
+    return <h1>Loading...</h1>
+  }
+  const genres = movie.genres.map(genre => <span key={genre}>{genre}</span>)
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        {genres}
       </main>
     </>
   );
